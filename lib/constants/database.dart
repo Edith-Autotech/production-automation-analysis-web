ebc35a1a97289a0f3246d5590852056979b54ee7
Omkar Tralsawala <omkar0406@gmail.com>
2022-07-16 16:28:03 +0530

    url changes

--- lib/constants/database.dart
+++ lib/constants/database.dart
@@ -1,5 +1,4 @@
-const String configUrl = "http://paa-env-v2.eba-uxtyxgzp.ap-south-1.elasticbeanstalk.com/graphql";
-// https://production-auto-analysis.herokuapp.com/graphql";
+const String configUrl = "https://production-automation-analysis-backend-lv3y5meoqq-as.a.run.app/graphql";
 const String production = "production";
 const String standby = "standby";
 const String idle = "idle";
